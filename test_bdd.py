a2163e65e20528b1d7c91430f25b09edf3015668
Yifei Xu <yifei.xu@tum.de>
2024-02-14 00:45:16 +0100

    change

--- test_bdd.py
+++ test_bdd.py
@@ -7,18 +7,17 @@ from torchvision.models.detection import FasterRCNN_ResNet50_FPN_Weights
 import torchvision
 # Assuming you have defined your model
 model = fasterrcnn_resnet50_fpn(weights = None) # Your PyTorch model
-model = torchvision.models.detection.fasterrcnn_resnet50_fpn(weights=torchvision.models.detection.FasterRCNN_ResNet50_FPN_Weights.DEFAULT)
+#model = torchvision.models.detection.fasterrcnn_resnet50_fpn(weights=torchvision.models.detection.FasterRCNN_ResNet50_FPN_Weights.DEFAULT)
 
-""" output_dir = "/home/yifei/bdd100k/network_faster_rcnn"  # Directory containing the model files
+checkpoint_path = '/home/yifei/bdd100k/network_faster_rcnn/checkpoint.pth'
 
-# Loop through each model file in the output directory
-for epoch in range(26):  # Assuming you have model_0.pth to model_25.pth
-    # Construct the path to the model file for the current epoch
-    model_file_path = os.path.join(output_dir, f"model_{epoch}.pth")
+if os.path.exists(checkpoint_path):
+    # Load the checkpoint
+    checkpoint = torch.load(checkpoint_path, map_location=torch.device('cpu'))  # Load to CPU if needed
     
-    # Load the weights from the model file
-    checkpoint = torch.load(model_file_path, map_location=torch.device('cpu'))  # Load to CPU if needed
-    model.load_state_dict(checkpoint["model"]) """
+    # Load the model state dictionary from the checkpoint
+    model.load_state_dict(checkpoint["model"])
+
 
 model.eval()
 
