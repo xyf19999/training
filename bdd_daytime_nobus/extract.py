8f23688665edf67b080ff429024aa2a5900e2074
Yifei Xu <yifei.xu@tum.de>
2024-05-11 03:34:49 +0200

    debug

--- bdd_daytime_nobus/extract.py
+++ bdd_daytime_nobus/extract.py
@@ -21,7 +21,7 @@ print(len(modified_data))
 modified_data_name = [element['name'] for element in modified_data]
 
 with open(f'/home/yifei/bdd_daytime_no_bus/bdd_sunny_no_bus_{usecase}.json', "w") as json_file:
-    json.dump(original_data, json_file, indent=2)
+    json.dump(modified_data, json_file, indent=2)
 
 for image in modified_data_name:
     src = os.path.join(source_folder_train, image)
