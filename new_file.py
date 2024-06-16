d7ca3b536fdd107d09c39603fc3ab7e688151cbd
Yifei Xu <yifei.xu@tum.de>
2024-06-16 03:38:01 +0200

    de

--- new_file.py
+++ new_file.py
@@ -13,7 +13,7 @@ def extract_first_column(file_path):
             result.append(index)
     print(len(result))
     return result
-included_image_val = extract_first_column('/home/yifei/monitizer/monitizer/benchmark_object_detection/specifications/bdd100k/generated_OOD_region_val.txt')
+included_image_val = extract_first_column('/home/yifei/monitizer/monitizer/benchmark_object_detection/specifications/bdd_coco/generated_OOD_region_val.txt')
 
 usecase = 'val'
 
@@ -23,7 +23,7 @@ def remove_labels(data):
             entry['labels'] = [label for label in entry['labels'] if label.get('category') not in ['drivable area', 'lane']]
     return data
 
-with open(f'/home/yifei/bdd100k/bdd100k_labels_images_val.json', 'r') as input_file:
+with open(f'/home/yifei/bdd_coco_old/bdd100k_labels_images_val.json', 'r') as input_file:
     data_val = json.load(input_file)
 
 data_val = remove_labels(data_val)
@@ -31,8 +31,8 @@ data_val = remove_labels(data_val)
 data_remaining_name = [data_val[i]['name'] for i in included_image_val]
 print(len(data_remaining_name))
 
-source_folder = '/home/yifei/bdd100k/val'
-destination_path = '/home/yifei/bdd100k/val_for_optimization'
+source_folder = '/home/yifei/bdd_coco_old/val2017'
+destination_path = '/home/yifei/bdd_coco_old/val_for_optimization'
 for file in data_remaining_name:
     source_path_new = os.path.join(source_folder, file)
     destination_path_new = os.path.join(destination_path, file)
